c06955cbebfcbf7b1512e10e425cdf9b2685367e
NITHIN N <NITHIN N>
2017-03-15 11:03:39 +0530

    slight ui changes

--- HrmsMvc/Scripts/Helpers/Common.js
+++ HrmsMvc/Scripts/Helpers/Common.js
@@ -3,7 +3,7 @@ const WARNING = 2;
 const ERROR = 3;
 const CONFIRM = 4;
 const months = JSON.parse('{ "January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12 }');
-
+const hostDomain = document.domain;
 
 function showMessageBox(type, message) {
 
